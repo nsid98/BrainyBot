9991ab08b154a954cddd0d764a9884db2d62e2c2
Sidharth Nayyar <sidharthnayyar1@hotmail.com>
2023-02-07 11:41:16 -0800

    updates

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const { App } = require('@slack/bolt');
 // Initializes your app with your bot token and signing secret
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true,
+  appToken: process.env.SLACK_APP_TOKEN,
+  port: process.env.PORT || 3000
+});
+
+app.message('hello', async ({ message, say }) => {
+  // say() sends a message to the channel where the event was triggered
+  await say(`Hey there this has changed now <@${message.user}>!`);
 });
 
 (async () => {
