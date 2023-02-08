35d962d2fd9a2bc4e24239e3f75045a4fa10fe54
Sidharth Nayyar <sidharthnayyar1@hotmail.com>
2023-02-07 16:35:35 -0800

    finish first day

--- app.js
+++ app.js
@@ -12,30 +12,47 @@ const app = new App({
 });
 
 const configuration = new Configuration({
+ 
 });
 
 const openai = new OpenAIApi(configuration);
+var completion;
+app.event('app_mention', async({event, context, client, say}) => {
+  try {
 
-// const completion = await openai.createCompletion({
-//   model: "text-davinci-003",
-//   prompt: `Summarize the Harry Potter series in a paragraph.`,
-//   temperature: 0.6,
-// });
+    const result = await client.conversations.replies({
+      channel: event.channel,
+      ts: event.thread_ts
+    });
 
-// console.log(completion.data.choices[0].text);
+    var text = "";
+    for(var i = 0; i < result.messages.length-1; i++){
+      const userResult = await client.users.info({
+        user: result.messages[i].user
+      });
+      text= text+ `${userResult.user.real_name} says ${result.messages[i].text} . `;
 
-app.message('hello', async ({ message, say }) => {
+      if(i==result.messages.length-2){
+        console.log(text)
+        completion = await openai.createCompletion({
+          model: "text-davinci-003",
+          prompt: `summarize the following conversation in point form: \n ${text}`,
+          temperature: 0.7,
+          max_tokens: 256,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0,
+          });
+        console.log(completion.data.choices)
+        await say("Here is the answer, " + completion.data.choices[0].text)
+      }
+    }
+  }
   
-  // say() sends a message to the channel where the event was triggered
-  const completion = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: message.text,
-    temperature: 0.6,
-  });
-  console.log(completion.data.choices)
-  await say(`Here is the answer, \n ${completion.data.choices[0].text}`);
+  catch (error) {
+    console.error(error);
+  }
 });
-
 (async () => {
   await app.start(process.env.PORT || 3000);
 
